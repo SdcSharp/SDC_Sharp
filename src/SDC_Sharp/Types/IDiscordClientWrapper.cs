36e37e3011baa87c4708b13d4fd3e5a5bfaaae99
Alexander <satan1c@ukr.net>
2021-07-15 14:51:55 +0300

    2.0.0

---
+++ src/SDC_Sharp/Types/IDiscordClientWrapper.cs
@@ -0,0 +1,9 @@
+﻿namespace SDC_Sharp
+{
+    public interface IDiscordClientWrapper
+    {
+        public int ShardCount { get; }
+        public int ServersCount { get; }
+        public ulong CurrentUserId { get; }
+    }
+}
