286ec36043adc4e332faff457a5c00546717f37d
Alexander <satan1c@ukr.net>
2022-06-15 11:29:35 +0300

    Removing unnecessary classes

--- Types/BaseService.cs
+++
@@ -1,8 +0,0 @@
-namespace SDC_Sharp.Types
-{
-    public abstract class BaseService
-    {
-        protected readonly SdcSharpClient Client;
-        protected BaseService(SdcSharpClient client) => Client = client;
-    }
-}
--- Types/Monitoring/BaseGuildPlace.cs
+++
@@ -1,9 +0,0 @@
-using SDC_Sharp.Types.Interfaces;
-
-namespace SDC_Sharp.Types.Monitoring
-{
-    public class BaseGuildPlace : IGuildPlace
-    {
-        public ulong Place { get; set; }
-    }
-}
