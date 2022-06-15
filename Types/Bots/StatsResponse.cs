378559abde40f99253c374f88262f2de591e6968
Alexander <satan1c@ukr.net>
2022-06-15 11:44:25 +0300

    class rename

--- Types/Bots/StatsResponse.cs
+++ Types/Bots/StatsResponse.cs
@@ -2,7 +2,7 @@ using SDC_Sharp.Types.Interfaces;
 
 namespace SDC_Sharp.Types.Bots
 {
-    public class BaseStatsResponse : IStatsResponse
+    public class StatsResponse : IStatsResponse
     {
         public string Status { get; set; }
     }
