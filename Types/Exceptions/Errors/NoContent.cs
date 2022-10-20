e7e72987ddfc25ac1190ef997bacf1d987d908cd
Alexander <satan1c@ukr.net>
2022-10-20 15:08:38 +0300

    NoContent error type

---
+++ Types/Exceptions/Errors/NoContent.cs
@@ -0,0 +1,11 @@
+using SDC_Sharp.Types.Interfaces;
+
+namespace SDC_Sharp.Types.Exceptions.Errors
+{
+	public class NoContent : ApiErrorException
+	{
+		internal NoContent(IError response) : base(response)
+		{
+		}
+	}
+}
