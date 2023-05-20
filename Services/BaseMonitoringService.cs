73e7a7670987f1a641cc07ab27007e58a82f07ce
Alexander <satan1c@ukr.net>
2023-05-20 13:45:29 +0300

    reformat

--- Services/BaseMonitoringService.cs
+++ Services/BaseMonitoringService.cs
@@ -1,35 +1,34 @@
 using System.Threading.Tasks;
 using SDC_Sharp.Types.Interfaces;
 
-namespace SDC_Sharp.Services
+namespace SDC_Sharp.Services;
+
+public abstract class BaseMonitoringService
 {
-	public class BaseMonitoringService
+	protected readonly ISdcSharpClient Client;
+
+	public BaseMonitoringService(ISdcSharpClient client)
 	{
-		protected readonly SdcSharpClient Client;
+		Client = client;
+	}
 
-		public BaseMonitoringService(SdcSharpClient client)
-		{
-			Client = client;
-		}
+	protected async Task<T> GetGuild<T>(ulong guildId) where T : IGuild
+	{
+		return await Client.GetRequest<T>("guild/" + guildId);
+	}
 
-		protected async Task<T> GetGuild<T>(ulong guildId) where T : IGuild
-		{
-			return await Client.GetRequest<T>("guild/" + guildId);
-		}
+	protected async Task<T> GetGuildPlace<T>(ulong guildId) where T : IGuildPlace
+	{
+		return await Client.GetRequest<T>("guild/" + guildId + "/place");
+	}
 
-		protected async Task<T> GetGuildPlace<T>(ulong guildId) where T : IGuildPlace
-		{
-			return await Client.GetRequest<T>("guild/" + guildId + "/place");
-		}
+	protected async Task<T> GetGuildRates<T>(ulong guildId)
+	{
+		return await Client.GetRequest<T>("guild/" + guildId + "/rated");
+	}
 
-		protected async Task<T> GetGuildRates<T>(ulong guildId)
-		{
-			return await Client.GetRequest<T>("guild/" + guildId + "/rated");
-		}
-
-		protected async Task<T> GetUserRates<T>(ulong userId)
-		{
-			return await Client.GetRequest<T>("user/" + userId + "/rated");
-		}
+	protected async Task<T> GetUserRates<T>(ulong userId)
+	{
+		return await Client.GetRequest<T>("user/" + userId + "/rated");
 	}
 }
